0e876d135c34dee99e6ed8cbd568071b109ecd4a
Miko-29 <muskan2912002@gmail.com>
2026-01-19 14:13:35 +0530

    initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ShopFlash',
+  webDir: 'www'
+};
+
+export default config;
