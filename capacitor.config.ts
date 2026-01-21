0a16d8ca6c0be42eb12a4e6d501b4e8878d358d1
Miko-29 <muskan2912002@gmail.com>
2026-01-21 12:15:02 +0530

    first relase

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.shopflash.app',
   appName: 'ShopFlash',
-  webDir: 'www'
+  webDir: 'www',
 };
 
 export default config;
